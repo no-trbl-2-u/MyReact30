653d7a1624d2c9724dcfa1984c0245b6521a01f9
PN143 <PN143@TJs-Desktop.home>
2018-11-26 01:33:03 -0500

    Added click functionality by passing it a function that takes a keyCode

--- 01-drum-kit/src/DrumSet/DrumPad/DrumPad.jsx
+++ 01-drum-kit/src/DrumSet/DrumPad/DrumPad.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
 const DrumPad = (props) => {
-  const {dataKey, kbdKey, sound} = props;
+  const {dataKey, kbdKey, sound, handleClick} = props;
   return (
     <React.Fragment>
-      <div data-key={ dataKey } className="key">
+      <div 
+        data-key={ dataKey } 
+        className="key"
+        onClick={ () => handleClick(dataKey) }
+      >
         <kbd>{ kbdKey }</kbd>
         <span className="sound">{ sound }</span>
       </div>
--- 01-drum-kit/src/DrumSet/DrumSet.jsx
+++ 01-drum-kit/src/DrumSet/DrumSet.jsx
@@ -5,71 +5,67 @@ class DrumSet extends Component {
 
   constructor() {
     super();
-    this.handleAudioKBD = this.handleAudioKBD.bind(this);
-    this.handleAudioClick = this.handleAudioClick.bind(this);
-    this.playAudio = this.playAudio.bind(this);
+    this.handleKBD = this.handleKBD.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  handleAudioKBD(event) {
+  // handleKBD :: Event -> *Side-Effect*
+  handleKBD(event) {
+
     // Isolate each pad via the keyboard KeyCode
     const key = document.querySelector(`.key[data-key="${event.keyCode}"`);
     const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
     
-    if(!audio) return; // Null any keys w/o sound
+    // Null any unused key Presses
+    if(!audio) return;
 
     this.handleClassNames(key);
     this.playAudio(audio);
   }
 
-  handleAudioClick(event) {
-    // const key = document.querySelector(`.key[data-key="${event.target}]`)
-    const target = event.target
-    console.log(target);
-    if(target === 'div'){
-      // const dataKey = event.target.getAttribute('data-key')
-      // const audio = document.querySelector(`audio[data-key="${dataKey}"]`)
-      // this.playAudio(audio);
-    }
-  }
+  // handleClick :: keyCode -> *Side-Effect*
+  handleClick(keyCode) {
 
-  playAudio(audio) {
-    audio.currentTime = 0; // Rewind to start of sample
-    audio.play(); // Play the .wav file associated w/ key
-  }
+    // Isolate each pad via the passed KeyCode
+    const key = document.querySelector(`.key[data-key="${keyCode}"`);
+    const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
 
+    this.handleClassNames(key);
+    this.playAudio(audio);
+  }
+  
+  // handleClassNames :: HTMLElement -> * Changes CSS
   handleClassNames(key) {
     key.className = 'key playing';                         // ADD
     setTimeout(() => key.classList.remove('playing'), 100) // REMOVE
   }
 
-
-  componentDidMount() {
-    const keys = document.querySelectorAll('.eachPad');
-
-    // Handle Keyboard
-    document.addEventListener('keydown', event => {
-      this.handleAudioKBD(event);
-    })
-
-    // Handle Clicking
-    keys.forEach(key => key.addEventListener('click', event => {
-      this.handleAudioClick(event);
-    }))
-
+  // playAudio :: <audio> -> * Plays Audio Clip
+  playAudio(audio) {
+    audio.currentTime = 0; // Rewind to start of sample
+    audio.play(); // Play the .wav file associated w/ key
   }
 
-  render() { 
+  componentDidMount() {
+    // Listen for and handle Keyboard Events
+    document.addEventListener('keydown', event => {
+      this.handleKBD(event);
+    })
+  }
+
+  render() {
+    const { keyData } = this.props;
     return (
       <div className="keys-container">
 
-      { this.props.keyData.map((each, index) => (
-        <div className='eachPad' key={ index }>
-
+      {/* Map over  */}
+      { keyData.map((each, index) => (
+        <div key={ index }>
           <DrumPad
             dataKey={ each.dataKey }
             kbdKey={ each.kbdKey }
             sound={ each.sound }
-            clickable='true'
+            handleClick={ this.handleClick }
           />
 
           <audio
